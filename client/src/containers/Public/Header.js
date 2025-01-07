62a6403c557eafa1b42e4ed74e366c6ee14206f2
NguyenHoangVu1402 <nguyenhoangvu140201@gmail.com>
2025-01-07 21:32:39 +0700

    update

--- client/src/containers/Public/Header.js
+++ client/src/containers/Public/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
     }, [navigate]);
 
     return (
-        <div className="w-1100">
+        <div className="w-3/5">
             <div className="w-full flex items-center justify-between">
             <Link to={'/'}>
             <img src={logo} alt="logo" className="w-[240px] h-[70px] object-contain"/>
